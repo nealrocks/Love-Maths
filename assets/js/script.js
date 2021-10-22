fdde3731e29a30164b29145c88738dfb334a3740
nealrocks <obrienneal1982@hotmail.com>
2021-10-22 06:53:20 +0000

    Add score to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,8 +45,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Awwww.... youu answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -59,21 +61,32 @@ function checkAnswer() {
  */
 function calculateCorrectAnswer() {
 
-        let operand1 = parseInt(document.getElementById('operand1').innertext);
-        let operand2 = parseInt(document.getElementById('operand2').innertext);
-        let operator = document.getElementById("operator").innertext;
+        let operand1 = parseInt(document.getElementById('operand1').innerText);
+        let operand2 = parseInt(document.getElementById('operand2').innerText);
+        let operator = document.getElementById("operator").innerText;
 
         if (operator === "+") {
             return [operand1 + operand2, "addition"];
         } else {
             alert(`Unimplemented operator ${operator}`);
-            throw (`Unimplemented operator ${operator}.Aborting!`);
+            throw `Unimplemented operator ${operator}.Aborting!`;
         }
 }
+/**
+ * Gets the current score from the DOM and increment it by 1 
+ */
 function incrementScore() {
 
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+
 }
+/**
+ * Gets the current tally of incorrect answers from the DOM and increment it by 1
+ */
 function incrementWrongAnswer() {
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 function displayAdditionQuestion(operand1, operand2) {
@@ -82,8 +95,10 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "+";
 
 }
-function displaySubtractQuestion() {
-
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "-";
 }
 function displayMultiplyQuestion() {
 
